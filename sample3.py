815e3f73846af7a69deb3c9f0f4bd2e1aefb93c6
Albert Sun <albert@albertsun.info>
2011-07-27 15:04:48 -0400

    added step 5

--- sample3.py
+++ sample3.py
@@ -7,7 +7,10 @@ for line in myfile:
     # if it finds it, it returns the index where it occurs. if it's not found it returns -1
     if (line.find("CU") != -1):
         print line
+
+        # this increments the value of count by 1
         count += 1
 
+# cleaning up by closing the file we opened
 myfile.close()
 print count
