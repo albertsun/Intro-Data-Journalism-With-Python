09cd2b90e4153dc13680ecbee8a4d710a428c4b5
Albert Sun <albert@albertsun.info>
2011-07-26 22:17:12 -0400

    parsing cuny city colleges course listing with BeautifulSoup as a demo, and getting the same type of course breakdown by class times

--- parse_citycollege.py
+++ parse_citycollege.py
@@ -2,18 +2,40 @@
 # -*- coding: utf-8 -*-
 
 from BeautifulSoup import BeautifulSoup
+import re
+from collections import defaultdict
+from pprint import pprint
 
-def parse(filename):
-    soup = BeautifulSoup(open(filename))
 
-    
+find_cuny_hours = re.compile(r'(\d{1,2}(:\d\d)?)\s*-\s*((\d{1,2}(:\d\d)?)\s*(([AP]M)|NOON))')
 
 if __name__ == "__main__":
-   #parse("citycollege/registrar.html")
-   if not soup:
-       soup = BeautifulSoup(open("citycollege/registrar.html"))
-   else:
-       print "soup already parsed"
+
+   soup = BeautifulSoup(open("citycollege/registrar.html"))
    
-   # print soup.find("font", "orange")
-   print len(soup.findAll("font", {"class": "orange"})) #counts number of occurences of font tag with class "orange"
+   starts = []
+   ends = []
+
+   coursetables = soup.findAll("table", {"class":"coursetable"})
+   for course in coursetables:
+       classtimes = course.findAll("td", text=re.compile(r'\d{1,2}:\d\d'))
+       for c in classtimes:
+           m = find_cuny_hours.search(c)
+           if m:
+               x = m.groups()
+               start = x[0]
+               end = x[2]
+               starts.append(start)
+               ends.append(end)
+               print start,end
+           else:
+               print "No match found"
+               print c
+   print "found %(count)d total class times" % { "count": len(starts) }
+   
+   # dividing up the classes we've found into different timeslots to count them
+   timeslots = defaultdict(int)
+   for i in range(len(starts)):
+       timeslots[starts[i]+"-"+ends[i]] += 1
+
+   pprint(sorted(timeslots.items(), key=lambda x:x[1], reverse=True))
